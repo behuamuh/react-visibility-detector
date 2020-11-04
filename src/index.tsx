de6e668e064da54240d2c41b297b4704b2ac6541
Behuamuh <sven1980@list.ru>
2020-11-04 11:08:12 +0300

    added export hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 import VisibilityDetector from './components/VisibilityDetector';
 
+export { default as useChangeVisibility } from './hooks/useChangeVisibility';
+
 export default VisibilityDetector;
