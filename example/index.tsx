35d7195bbee8f51c7a451da4156eea9052951a18
Behuamuh <sven1980@list.ru>
2020-11-04 05:27:35 +0300

    changed example

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
 
-const App = () => {
+import VisibilityDetector from '../.';
+
+function handleVisibilityChange({ visible, directionX, directionY }) {
+  console.log('Element is now %s', visible ? 'visible' : 'hidden');
+  console.log('Horizontal direction %s', directionX); // left, right, or none if no changed or initial
+  console.log('Vertical direction %s', directionY); // up, down or none if no changed or initial
+}
+
+// scroll to bottom and see the console
+
+function MyComponent(props) {
   return (
-    <div>
-      <Thing />
+    <div style={{
+      height: 2000,
+      display: "flex",
+      alignItems: "flex-end",
+    }}>
+      <VisibilityDetector onVisibilityChange={handleVisibilityChange}>
+        <div>...content goes here...</div>
+      </VisibilityDetector>
     </div>
   );
-};
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
